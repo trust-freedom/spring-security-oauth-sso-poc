cc86ba1a83e752b913c8498754949127d523053f
trust-freedom <trust_freedom@sina.com>
2019-05-27 14:24:31 +0800

    修改项目名、域名

--- springboot 1.x/authorization-server-1/src/main/java/com/freedom/AuthorizationServerApplication.java
+++ springboot 1.x/authorization-server-1/src/main/java/com/freedom/AuthorizationServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UaaServiceApplication {
+public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UaaServiceApplication.class, args);
+        SpringApplication.run(AuthorizationServerApplication.class, args);
     }
 
 }
